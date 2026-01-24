e584e88a2325bb7e8e1e1d2781e71fe09d8311b1
Andrew Pollard <9512053+Andrew-Pollard@users.noreply.github.com>
2026-01-24 12:29:54 +0000

    Changed references from System.Numerics types to Invicta.Numerics types in Vector3D extensions.

--- Invicta.Numerics.Vectors/Vector3D.Extensions.cs
+++ Invicta.Numerics.Vectors/Vector3D.Extensions.cs
@@ -9,31 +9,31 @@ namespace Invicta.Numerics
 {
     public static unsafe partial class Vector
     {
-        /// <summary>Reinterprets a <see cref="Vector3" /> as a new <see cref="Vector2" />.</summary>
+        /// <summary>Reinterprets a <see cref="Vector3D" /> as a new <see cref="Vector2D" />.</summary>
         /// <param name="value">The vector to reinterpret.</param>
-        /// <returns><paramref name="value" /> reinterpreted as a new <see cref="Vector2" />.</returns>
-        public static Vector2 AsVector2(this Vector3 value) => value.AsVector128().AsVector2();
+        /// <returns><paramref name="value" /> reinterpreted as a new <see cref="Vector2D" />.</returns>
+        public static Vector2D AsVector2(this Vector3D value) => value.AsVector128().AsVector2D();
 
-        /// <summary>Converts a <see cref="Vector3" /> to a new <see cref="Vector4" /> with the new elements zeroed.</summary>
+        /// <summary>Converts a <see cref="Vector3D" /> to a new <see cref="Vector4D" /> with the new elements zeroed.</summary>
         /// <param name="value">The vector to convert.</param>
-        /// <returns><paramref name="value" /> converted to a new <see cref="Vector4" /> with the new elements zeroed.</returns>
-        public static Vector4 AsVector4(this Vector3 value) => value.AsVector128().AsVector4();
+        /// <returns><paramref name="value" /> converted to a new <see cref="Vector4D" /> with the new elements zeroed.</returns>
+        public static Vector4D AsVector4(this Vector3D value) => value.AsVector128().AsVector4D();
 
-        /// <summary>Converts a <see cref="Vector3" /> to a new <see cref="Vector4" /> with the new elements undefined.</summary>
+        /// <summary>Converts a <see cref="Vector3D" /> to a new <see cref="Vector4D" /> with the new elements undefined.</summary>
         /// <param name="value">The vector to convert.</param>
-        /// <returns><paramref name="value" /> converted to a new <see cref="Vector4" /> with the new elements undefined.</returns>
-        public static Vector4 AsVector4Unsafe(this Vector3 value) => value.AsVector128Unsafe().AsVector4();
+        /// <returns><paramref name="value" /> converted to a new <see cref="Vector4D" /> with the new elements undefined.</returns>
+        public static Vector4D AsVector4Unsafe(this Vector3D value) => value.AsVector128Unsafe().AsVector4D();
 
-        /// <inheritdoc cref="ExtractMostSignificantBits(Vector4)" />
+        /// <inheritdoc cref="ExtractMostSignificantBits(Vector4D)" />
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint ExtractMostSignificantBits(this Vector3 vector) => vector.AsVector128().ExtractMostSignificantBits();
+        public static uint ExtractMostSignificantBits(this Vector3D vector) => vector.AsVector128().ExtractMostSignificantBits();
 
-        /// <inheritdoc cref="GetElement(Vector4, int)" />
+        /// <inheritdoc cref="GetElement(Vector4D, int)" />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float GetElement(this Vector3 vector, int index)
+        public static float GetElement(this Vector3D vector, int index)
         {
-            if ((uint)index >= Vector3.ElementCount)
+            if ((uint)index >= Vector3D.ElementCount)
             {
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.index);
             }
@@ -44,7 +44,7 @@ namespace Invicta.Numerics
         /// <param name="source">The vector that will be stored.</param>
         /// <param name="destination">The destination at which <paramref name="source" /> will be stored.</param>
         [CLSCompliant(false)]
-        public static void Store(this Vector3 source, float* destination) => source.StoreUnsafe(ref *destination);
+        public static void Store(this Vector3D source, float* destination) => source.StoreUnsafe(ref *destination);
 
         /// <summary>Stores a vector at the given 8-byte aligned destination.</summary>
         /// <param name="source">The vector that will be stored.</param>
@@ -52,14 +52,14 @@ namespace Invicta.Numerics
         /// <exception cref="AccessViolationException"><paramref name="destination" /> is not 8-byte aligned.</exception>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void StoreAligned(this Vector3 source, float* destination)
+        public static void StoreAligned(this Vector3D source, float* destination)
         {
-            if (((nuint)destination % (uint)(Vector3.Alignment)) != 0)
+            if (((nuint)destination % (uint)(Vector3D.Alignment)) != 0)
             {
                 ThrowHelper.ThrowAccessViolationException();
             }
 
-            *(Vector3*)destination = source;
+            *(Vector3D*)destination = source;
         }
 
         /// <summary>Stores a vector at the given 8-byte aligned destination.</summary>
@@ -68,13 +68,13 @@ namespace Invicta.Numerics
         /// <exception cref="AccessViolationException"><paramref name="destination" /> is not 8-byte aligned.</exception>
         /// <remarks>This method may bypass the cache on certain platforms.</remarks>
         [CLSCompliant(false)]
-        public static void StoreAlignedNonTemporal(this Vector3 source, float* destination) => source.StoreAligned(destination);
+        public static void StoreAlignedNonTemporal(this Vector3D source, float* destination) => source.StoreAligned(destination);
 
         /// <summary>Stores a vector at the given destination.</summary>
         /// <param name="source">The vector that will be stored.</param>
         /// <param name="destination">The destination at which <paramref name="source" /> will be stored.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void StoreUnsafe(this Vector3 source, ref float destination)
+        public static void StoreUnsafe(this Vector3D source, ref float destination)
         {
             ref byte address = ref Unsafe.As<float, byte>(ref destination);
             Unsafe.WriteUnaligned(ref address, source);
@@ -86,25 +86,25 @@ namespace Invicta.Numerics
         /// <param name="elementOffset">The element offset from <paramref name="destination" /> from which the vector will be stored.</param>
         [CLSCompliant(false)]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void StoreUnsafe(this Vector3 source, ref float destination, nuint elementOffset)
+        public static void StoreUnsafe(this Vector3D source, ref float destination, nuint elementOffset)
         {
             destination = ref Unsafe.Add(ref destination, (nint)elementOffset);
             Unsafe.WriteUnaligned(ref Unsafe.As<float, byte>(ref destination), source);
         }
 
-        /// <inheritdoc cref="ToScalar(Vector4)" />
+        /// <inheritdoc cref="ToScalar(Vector4D)" />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float ToScalar(this Vector3 vector) => vector.AsVector128Unsafe().ToScalar();
+        public static float ToScalar(this Vector3D vector) => vector.AsVector128Unsafe().ToScalar();
 
-        /// <inheritdoc cref="WithElement(Vector4, int, float)" />
+        /// <inheritdoc cref="WithElement(Vector4D, int, float)" />
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3 WithElement(this Vector3 vector, int index, float value)
+        public static Vector3D WithElement(this Vector3D vector, int index, float value)
         {
-            if ((uint)index >= Vector3.ElementCount)
+            if ((uint)index >= Vector3D.ElementCount)
             {
                 ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.index);
             }
-            return vector.AsVector128Unsafe().WithElement(index, value).AsVector3();
+            return vector.AsVector128Unsafe().WithElement(index, value).AsVector3D();
         }
     }
 }
