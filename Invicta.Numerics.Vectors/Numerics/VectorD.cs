19f109d38973f6ca39519ce6b5afd1a0e19aa2e7
Andrew Pollard <9512053+Andrew-Pollard@users.noreply.github.com>
2026-01-28 21:34:35 +0000

    Added VectorD.cs to give VectorD an XML doc comment.

---
+++ Invicta.Numerics.Vectors/Numerics/VectorD.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Andrew Pollard, .NET Foundation and Contributors.
+// Licensed under the MIT license - see README.md for details.
+
+namespace Invicta.Numerics
+{
+    /// <summary>Provides a collection of static methods for creating, manipulating, and otherwise operating on generic vectors.</summary>
+    public static partial class VectorD
+    {
+        // No point copying the contents of the original Vector.cs - we just need somewhere to put the class
+        // XML doc comment that doesn't interfere with the line numbers in the *.Extensions.cs files.
+    }
+}
